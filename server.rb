ae0db8bb34733d60003f824b6a27f1bb0a9922ee
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-06-03 20:40:34 +0530

    page rendering

--- server.rb
+++ server.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 
-get '/' do
-    "Hello World!"
-end
+# # Example of setting up a static folder
+# set :public_folder, File.dirname(__FILE__) + '/static'
 
-# Use of params and query in url
-get '/:cheese' do
-    content_type :json
-    return { :cheese => params['cheese'], :query => params['query']}.to_json
-end
+# get '/' do
+#     "Hello World!"
+# end
+
+# # Use of params and query in url
+# get '/:cheese' do
+#     content_type :json
+#     return { :cheese => params['cheese'], :query => params['query']}.to_json
+# end
