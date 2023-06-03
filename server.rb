acbc488e9080c5627dbc69d59128f30c9849286e
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-06-03 12:51:13 +0530

    use of params

--- server.rb
+++ server.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 get '/' do
     "Hello World!"
 end
+
+# Use of params and query in url
+get '/:cheese' do
+    content_type :json
+    return { :cheese => params['cheese'], :query => params['query']}.to_json
+end
